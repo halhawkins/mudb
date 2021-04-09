e2f7f76d58cd1faa40fc8ec931eaf72bb86c552c
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-09 15:40:47 -0400

    experimenting with user profile on menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -39,9 +39,13 @@
           <img src="{{asset('/images/magnifying-glass-icon-20.png')}}">
         </button>
       </form>
+        @if(session()->has('username'))
+            <img src="{{session('avatar')}}" style="width:32px;height:auto;border-radius:50%;"><span>Hello {{session('username')}}</span>
+        @else
                     <!-- <input type="text" class="input" placeholder="search"><button type="submit"><i class="fa fa-search"></i></button> -->
-                    <a href="#" class="nav-item nav-link">Register</a>
-                    <a href="#" class="nav-item nav-link">Login</a>
+                    <a href="#" class="nav-item nav-link" data-toggle="modal" data-target="#register-dialog">Register</a>
+                    <a href="#" class="nav-item nav-link" data-toggle="modal" data-target="#login-modal">Login</a>
+        @endif
                 </div>
             </div>
         </div>    
@@ -62,7 +66,8 @@
         </div>
 
         <hr>
-        <footer>
+    </div>
+        <footer class="footer">
             <div class="row">
                 <div class="col-md-6">
                     <p>Copyright &copy; 2021 Musicor</p>
@@ -74,12 +79,104 @@
                 </div>
             </div>
         </footer>
-    </div>
+
     <!--
         =====================================
         End of Container
         =====================================
     -->
-    
+    <div id="register-modal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Register</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+
+                <form method="GET" action="{{url('/')}}/auth/redirect">
+                    @csrf
+                    <!-- <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="name" class="form-control p_input">
+                    </div>
+                    <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control p_input">
+                    </div>
+                    <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control p_input">
+                    </div> -->
+
+                    <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-block enter-btn">Register</button>
+                    </div>
+
+                    <p class="sign-up text-center">Already have an Account?<a href=""> Sign In</a></p>
+                    <p class="terms">By creating an account you are accepting our<a href="#"> Terms & Conditions</a></p>
+                </form>
+
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" role="dialog" id="register-dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Register</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div style="text-align:center;margin:4px;"><a class="btn btn-dark" href="{{url('/')}}/auth/redirect"><img src="{{url('/')}}/images/google-logo.png" style="width:32px;height:32px;margin-right:8px;">Register using Google</a></div>
+                    <div style="text-align:center;margin:4px;"><a class="btn btn-dark" href="{{url('/')}}/register"><img src="{{url('/')}}/images/register-user.png" style="width:32px;height:32px;margin-right:8px;">Create and account using email address.</a></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="login-modal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Login</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+
+
+                <form method="post" action="">
+                    @csrf
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="name" class="form-control p_input">
+                </div>
+                <div class="form-group">
+                    <label>Password *</label>
+                    <input type="password" name="password" class="form-control p_input">
+                </div>
+
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                </div>
+
+                <p class="sign-up">Don't have an Account?<a href=""> Sign Up</a></p>
+                </form>
+                                    
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
