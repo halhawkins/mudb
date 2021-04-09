5f09a9482117d4353ab675272f1cd9bad791c575
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-09 15:42:26 -0400

    experimenting with registration and auth routes

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,17 @@
 
  use Illuminate\Support\Facades\Route;
  use Illuminate\Http\Request;
+ use App\Http\Controllers\UserController;
+ use App\Http\Controllers\LoginController;
+ use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('newreleases');
 });
 
-Route::get('/hal', function () {
-    return view('hal');
-});
+Route::get('/hal', [UserController::class,'index']);
+
+Route::post('/register',[UserController::class,'register']);
 
 Route::get('/artist/{artistid}', function($artistid){
     return view('artist')->with('artistid',$artistid);
@@ -33,3 +36,24 @@ Route::get('/categories',function() {
     return response()->json($res);
 });
 
+Route::get('/auth/redirect', function () {
+    // dd($_REQUEST)
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/callback', function (Request $request) {
+    $user = Socialite::driver('google')->user();
+    $request->session()->put('username',$user['name']);
+    $request->session()->put('avatar',url('/')."/images/noartistimage.png");
+    return view("newreleases");
+    // $user->token
+});
+
+Route::get('/auth/user',function(){
+    $user = Socialite::driver('google')->user();
+    return response()->json($user);
+});
+
+Route::get('/register',function(){
+    return view('register');
+});
