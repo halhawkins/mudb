9b19838f50398c3ddd496e6d14794945fd8bd83e
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-17 15:57:24 -0400

    migrations for top lists in Spotify.

---
+++ database/migrations/2021_04_17_191845_spotify_viral50.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SpotifyViral50 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('spotify_viral50', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            // $table->collation('utf8_general_ci');
+            $table->bigIncrements('id');
+            $table->unsignedInteger('position');
+            $table->string('track_name');
+            $table->string('artist');
+            $table->unsignedInteger('streams');
+            $table->string('spotify_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('spotify_viral50');
+    }
+}
