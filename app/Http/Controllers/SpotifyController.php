5eec48c6f24a686e2c8f4a5993d4a619cc208b43
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-18 15:32:46 -0400

    corrected typo in output array

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -10,13 +10,23 @@ use Illuminate\Support\Carbon;
 class SpotifyController extends Controller
 {
     //
-    public function viral($page=1,$perPage=20){
+    public function viral($page=1,$perpage=20){
         $spotify_viral50 = spotify_viral50::select('spotify_data')->whereDate('created_at', Carbon::today())->get();
         $viral50For = date('Y-m-d');
         $count = count($spotify_viral50);
-        $chunks = array_chunk(json_decode(json_encode($spotify_viral50),true),$perPage);
+        $chunks = array_chunk(json_decode(json_encode($spotify_viral50),true),$perpage);
         $res = $chunks[$page-1];
         $total = array("viral50_for_date"=>$viral50For,"total_count"=>$count,"tracks"=>$res);
+        return response()->json($total);
+    }
+    //
+    public function top200($page=1,$perpage=20){
+        $top200 = spotify_top200::select('spotify_data')->whereDate('created_at', Carbon::today())->get();
+        $top200For = date('Y-m-d');
+        $count = count($top200);
+        $chunks = array_chunk(json_decode(json_encode($top200),true),$perpage);
+        $res = $chunks[$page-1];
+        $total = array("top200_for_date"=>$top200For,"total_count"=>$count,"tracks"=>$res);
         return response()->json($total);
     }
 }
