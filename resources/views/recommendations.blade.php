70f9996bf5b597bc69b28816423291b607ae2417
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-29 12:39:12 -0400

    Responsive improvements

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -99,16 +99,27 @@
             return ret;
         }
 
+        function large_view(){
+            $(".artist-cell").addClass("col-lg-3").addClass("col-md-4");
+            $(".artist-card,.info-container").removeClass('compact');
+            $(".info-container compact").removeClass("col-9 col-sm-10 col-xl-11").addClass("col-12");
+            $(".artist-image").removeClass("col-3 col-sm-2 col-xl-1").addClass("col-12");
+
+        }
+
+        function compact_view(){
+                $(".artist-cell").removeClass("col-lg-3").removeClass("col-md-4");
+                $(".artist-card,.info-container").addClass('compact');
+                $(".info-container").removeClass("col-12").addClass("col-9 col-sm-10 col-xl-11");
+                $(".artist-image").removeClass("col-12").addClass("col-3 col-sm-2 col-xl-1");             
+        }
+
         $(document).ready(function(){
             $(".dripicons-view-thumb").click(function(){
-                $(".artist-card,.info-container").removeClass('compact');
-                $(".info-container compact").removeClass("col-10").addClass("col-12");
-                $(".artist-image").removeClass("col-2").addClass("col-12");
+                large_view();
             });
             $(".dripicons-view-list-large").click(function(){
-                $(".artist-card,.info-container").addClass('compact');
-                $(".info-container").removeClass("col-12").addClass("col-10");
-                $(".artist-image").removeClass("col-12").addClass("col-2");
+                compact_view();
             });
             $(".viral-menu,.top200-menu,.new-releases-menu").removeClass("active");
             $(".recommendations-menu").addClass("active");
@@ -151,20 +162,21 @@
                         // releaseYear = "";
                         burl = "{{url('/')}}";
                         content = `
-                            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12"> 
+                            <div class="col-lg-3 col-md-4 col-sm-12 col-12 artist-cell"> 
                                 <div class="col-12 artist-card compact">
                                     <div class="row">
-                                            <div class="col-2 artist-image">
+                                            <div class="col-3 col-sm-2 col-xl-1 artist-image">
                                                 <a href="{{url('/')}}/track/` + track.id + `">
                                                 <img src="` + image + `" alt="album cover"></a>
                                             </div>
-                                            <div class="col-10 info-container compact">
+                                            <div class="col-9 col-sm-10 col-xl-11 info-container compact">
                                                     <h5>` + trackName + `</h5>
                                                     <span class="">` + albumName + ` (` + releaseYear + `)</span>
                                                             <em>`+ artists +`</em><br>
-                                                    <a href="` + spotifyUrl + `" title="Play on spotify"><img src="{{url('/')}}/images/Spotify_play.png" style="width:24px;height:auto;"> Play on Spotify</a><br/>
-                                                    <audio title="Audio preview" style="height:12px; width:90%;background-color:white; margin-left:5px;" src="` + previewUrl + `" type="audio/mpeg" controls disabled>I'm sorry. You're browser doesn't support HTML5 <code>audio</code>.</audio>
-                                            </div><!-- end info-container -->
+                                                    <a href="` + spotifyUrl + `" title="Play on spotify"><img src="{{url('/')}}/images/Spotify_play.png" style="width:24px;height:auto;"> Play on Spotify</a><br/>`
+                        if(previewUrl !== null) // surpress 404s loading missing preview track
+                            content +=              `<audio title="Audio preview" style="height:12px; width:90%;background-color:white; margin-left:5px;" src="` + previewUrl + `" type="audio/mpeg" controls disabled>I'm sorry. You're browser doesn't support HTML5 <code>audio</code>.</audio>`;
+                        content +=                  `</div><!-- end info-container -->
                                     </div>
                                 </div>
                             </div>`;
@@ -173,11 +185,13 @@
 
                     });
                     $("#tracks").append(`<div class="w-100"><div class="d-flex justify-content-center"><a class="page-link" href="{{url('/')}}/recommendations">More...</a></div>`);
+                    large_view();                
                 }
 
                     // followers = response.followers.total;
                     // tags = response.genres;
             });
+
         });
         
     </script>
