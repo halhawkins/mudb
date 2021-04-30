d6d70b8f263f49975024a4c942f85552d9b26957
Hal Hawkins <hal.hawkins@gmail.com>
2021-04-30 17:10:23 -0400

    added the view styles

--- resources/views/artist.blade.php
+++ resources/views/artist.blade.php
@@ -18,7 +18,55 @@
         @else
         viewstyle = "fat";
         @endif
+
+        function large_view(){
+        $(".artist-cell").addClass("col-lg-4").addClass("col-md-6");
+        $(".info-container compact").removeClass("col-9 col-sm-10 col-xl-12").addClass("col-12");
+        $(".artist-card,.info-container").removeClass('compact');
+        $(".artist-image").removeClass("col-3 col-sm-2 col-lg-2 col-xl-2").addClass("col-12");
+        $.ajaxSetup({
+                headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+            });
+            $.ajax({
+                type: "POST",
+                url: "{{url('/')}}/setviewstyle",
+                data: {viewstyle:"large"},
+                success: function (response) {
+                    viewstyle="large";
+                }
+            });
+
+    }
+
+    function compact_view(){
+        $(".artist-cell").removeClass("col-lg-4 col-md-6 col-sm-12").addClass("col-12");
+        $(".artist-card,.info-container").addClass('compact');
+        $(".info-container").removeClass("col-12").addClass("col-9 col-sm-10 col-xl-10");
+        $(".artist-image").removeClass("col-12").addClass("col-3 col-sm-2 col-lg-2 col-xl-2");             
+        $.ajaxSetup({
+                    headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                });
+                 $.ajax({
+                type: "POST",
+                url: "{{url('/')}}/setviewstyle",
+                data: {viewstyle:"compact"},
+                success: function (response) {
+                    viewstyle="compact";
+                }
+            });
+    }
+
         $(document).ready(function(){
+            $(".dripicons-view-thumb").click(function(){
+                large_view();
+            });
+            $(".dripicons-view-list-large").click(function(){
+                compact_view();
+            });
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -54,7 +102,7 @@
                     artistName = response.name;
                     $(".artist-jumbo").css("background-image",backgroundimage);
                     $(".artist-jumbo").css("background-size","cover");
-                    $("#artist-name-heading").html(artistName);
+                    $("#artist-name").html(artistName);
                     bioURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + encodeURIComponent(artistName) + "&api_key=40e7023497e3403fc3d672679eba6f03&format=json";
                     $.ajax({
                         type: "GET",
@@ -75,25 +123,42 @@
                                         image = album.images[1].url;
                                         releaseDate = new Date(album.release_date);
                                         releaseYear = releaseDate.getFullYear();
-                                        content = `<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                                                        <div class="col-12 artist-card">
-                                                        <img src="` + image + `" style="width:100%;height:auto;">
-                                                        <h5><a title="Album name" href="{{url('/')}}/album/` + album.id + `">` + album.name + `</a></h5>
-                                                        `
-                                        + "(" + releaseYear + `)<br/>
-                                        `;
+                                        spotifyUrl = album.uri;
+                                        albumArtists = "";
                                         $.each(album.artists, function(i,artguy) {
-                                            content = content + `<a title="Artist name" href="{{url('/')}}/artist/` + artguy.id + `">` + artguy.name + `</a>`;
+                                            albumArtists += `<a title="Artist name" href="{{url('/')}}/artist/` + artguy.id + `">` + artguy.name + `</a>`;
                                             if(album.artists.length > (i+1))
-                                                content = content + ", ";
+                                            albumArtists += ", ";
                                         })
-                                        content = content + `<br/><a href="` + album.uri + `" target="_blank"><img src="{{url('/')}}/images/Spotify_play.png" style="width:24px;height:auto;"> Play on Spotify</a>`;
-                                        content = content + `</div>
-                                                            </div>
-                                                `;
+                                        content = 
+                                        `
+                                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 artist-cell"> 
+
+                                        <div class="col-12 artist-card compact">
+                                            <div class="row">
+                                                <div class="col-3 col-sm-2 col-xl-2 artist-image">
+                                                    <a href="{{url('/')}}/album/` + album.id + `">
+                                                    <img src="` + image + `" alt="album cover"></a>
+                                                </div>
+                                                <div class="col-9 col-sm-10 col-xl-10 info-container compact">
+                                                    <h5 class="track-name">` + album.name + `</h5>
+                                                            <em>`+ albumArtists +`</em><br>
+                                                    <a href="` + spotifyUrl + `" title="Play on spotify"><img src="{{url('/')}}/images/Spotify_play.png" style="width:24px;height:auto;"> Play on Spotify</a><br/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`
+
+
+
+
                                         albs.append(content);                        
                                         });
                                         url = "{{url('/')}}/artist/" + artistID + "/";
+                                        if(viewstyle === "compact")
+                                            compact_view();       
+                                        else   
+                                            large_view();         
                                         $("#artist").append(paginate(url,totalAlbums,page,perPage,8)); 
 
                                         $.ajax({
@@ -132,6 +197,9 @@
 @endsection
 
 @section('mainbody')
+        <div class="col-md-12 toggle-bar"><h3 id="artist-name" class="panel-heading"></h3><em class="btn float-right icon dripicons-view-thumb" title="Full Size Panel View"></em><em class="btn float-right icon dripicons-view-list-large"  title="Compact View"></em>
+                <!-- #recent-releases filled in by ajax request handler -->
+            </div>
         <div class="col-md-12 artist-info primary-bg"><h3 id="artist-name-heading"></h3>
                 <div class="row primary-bg" id="artist" style="padding-bottom:0px;">
                     <div class="col-md-3 col-lg-3 col-xl-3 " style="padding-bottom:0px;">
